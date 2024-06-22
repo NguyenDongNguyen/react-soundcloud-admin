eebf43210703989c7ccf439c73e5a0534bc438dc
NguyenDongNguyen <ndn030802@gmail.com>
2024-06-22 11:17:53 +0700

    additional dashboard admin

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import CountUp from 'react-countup';
 import { Card, Col, Row, Statistic, notification } from 'antd';
 import { valueType } from 'antd/es/statistic/utils';
+import ReportCharts from './components/report/report.chart';
+import { TeamOutlined, CustomerServiceOutlined } from '@ant-design/icons';
 
 const ManageDashboardPage = () => {
     const [listUsers, setListUsers] = useState([]);
@@ -65,24 +67,87 @@ const ManageDashboardPage = () => {
             <Row gutter={[40, 40]}>
                 <Col span={10}>
                     <Card title="" bordered={false}>
-                        <Statistic
-                            title="All Users"
-                            value={listUsers.length}
-                            formatter={formatter}
-                        />
+                        <div
+                            style={{
+                                display: 'flex',
+                                alignItems: 'center',
+                                gap: '15px',
+                            }}
+                        >
+                            <TeamOutlined
+                                style={{
+                                    fontSize: '30px',
+                                    padding: '15px',
+                                    color: '#4154f1',
+                                    backgroundColor: '#e8e8e8',
+                                    borderRadius: '50%',
+                                }}
+                            />
+                            <Statistic
+                                title="All Users"
+                                value={listUsers.length}
+                                formatter={formatter}
+                                style={{
+                                    color: '#012970 !important',
+                                    fontWeight: '600',
+                                }}
+                            />
+                        </div>
                     </Card>
                 </Col>
                 <Col span={10}>
-                    <Card title="" bordered={false}>
-                        <Statistic
-                            title="All Tracks"
-                            value={listTracks.length}
-                            precision={2}
-                            formatter={formatter}
-                        />
+                    <Card title="" bordered={false} style={{ display: 'flex' }}>
+                        <div
+                            style={{
+                                display: 'flex',
+                                alignItems: 'center',
+                                gap: '15px',
+                            }}
+                        >
+                            <CustomerServiceOutlined
+                                style={{
+                                    fontSize: '30px',
+                                    padding: '15px',
+                                    color: '#4154f1',
+                                    backgroundColor: '#e8e8e8',
+                                    borderRadius: '50%',
+                                }}
+                            />
+                            <Statistic
+                                title="All Tracks"
+                                value={listTracks.length}
+                                precision={2}
+                                formatter={formatter}
+                                style={{
+                                    color: '#012970 !important',
+                                    fontWeight: '600',
+                                }}
+                            />
+                        </div>
                     </Card>
                 </Col>
             </Row>
+
+            <div
+                style={{
+                    marginTop: '30px',
+                    background: '#fff',
+                    padding: '0 20px 20px 20px',
+                }}
+            >
+                <h5
+                    style={{
+                        padding: '20px 0 5px 0',
+                        fontSize: '18px',
+                        fontWeight: '500',
+                        color: '#012970',
+                        letterSpacing: '1px',
+                    }}
+                >
+                    Report
+                </h5>
+                <ReportCharts />
+            </div>
         </div>
     );
 };
